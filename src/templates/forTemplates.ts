252ac8eca8ba54d91655b726ebabf45fde6338aa
filwaline <skywalker950925@gmail.com>
2019-03-05 16:21:55 +0800

    improvements to for templates

--- src/templates/forTemplates.ts
+++ src/templates/forTemplates.ts
@@ -19,46 +19,50 @@ abstract class BaseForTemplate extends BaseTemplate {
   protected isArrayLiteral = (node: ts.Node) => node.kind === ts.SyntaxKind.ArrayLiteralExpression
 }
 
-export class ForTemplate extends BaseForTemplate {
+export class ForLoopTemplate extends BaseForTemplate {
   buildCompletionItem (code: string, position: vsc.Position) {
     return CompletionItemBuilder
-      .create('for', code)
-      .description('for (let i = 0; i < expr.Length; i++)')
-      .replace(`for (let \${1:i} = 0; \${1} < \${2:{{expr}}}.length; \${1}++) {\n${getIndentCharacters()}\${0}\n}`, position, true)
+      .create('forloop', code)
+      // .description('for (let i = 0; i < expr.Length; i++)')
+      .description('for i in range(N):')
+      // .replace(`for (let \${1:i} = 0; \${1} < \${2:{{expr}}}.length; \${1}++) {\n${getIndentCharacters()}\${0}\n}`, position, true)
+      .replace(`for \${1:i} in range(\${2:{{expr}}}): \n${getIndentCharacters()}\${0}\n`, position, true)
       .build()
   }
 
-  canUse (node: ts.Node) {
-    return super.canUse(node) && !this.isArrayLiteral(node.parent)
-  }
+  // canUse (node: ts.Node) {
+  //   return super.canUse(node) && !this.isArrayLiteral(node.parent)
+  // }
 }
 
-export class ForOfTemplate extends BaseForTemplate {
+export class ForInTemplate extends BaseForTemplate {
   buildCompletionItem (code: string, position: vsc.Position) {
     return CompletionItemBuilder
-      .create('forof', code)
-      .description('for (let item of expr)')
-      .replace(`for (let \${1:item} of \${2:{{expr}}}) {\n${getIndentCharacters()}\${0}\n}`, position, true)
+      .create('forin', code)
+      // .description('for (let item of expr)')
+      .description('for item in expr:')
+      // .replace(`for (let \${1:item} of \${2:{{expr}}}) {\n${getIndentCharacters()}\${0}\n}`, position, true)
+      .replace(`for \${1:item} in \${2:{{expr}}}: \n${getIndentCharacters()}\${0}\n`, position, true)
       .build()
   }
 }
 
-export class ForEachTemplate extends BaseForTemplate {
-  buildCompletionItem (code: string, position: vsc.Position) {
-    return CompletionItemBuilder
-      .create('foreach', code)
-      .description('expr.forEach()')
-      .replace(`{{expr}}.forEach(\${1:item} => \${2})`, position, true)
-      .build()
-  }
+// export class ForEachTemplate extends BaseForTemplate {
+//   buildCompletionItem (code: string, position: vsc.Position) {
+//     return CompletionItemBuilder
+//       .create('foreach', code)
+//       .description('expr.forEach()')
+//       .replace(`{{expr}}.forEach(\${1:item} => \${2})`, position, true)
+//       .build()
+//   }
 
-  canUse (node: ts.Node) {
-    return super.canUse(node)
-  }
-}
+//   canUse (node: ts.Node) {
+//     return super.canUse(node)
+//   }
+// }
 
 export const build = () => [
-  new ForTemplate(),
-  new ForOfTemplate(),
-  new ForEachTemplate()
+  new ForLoopTemplate(),
+  new ForInTemplate(),
+  // new ForEachTemplate()
 ]
