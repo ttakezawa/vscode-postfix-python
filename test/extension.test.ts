2c1ca2ed57e2a1dad8fdf7e21c38e339b243a9d1
filwaline <skywalker950925@gmail.com>
2019-03-05 15:49:51 +0800

    mini fix

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -8,7 +8,7 @@ import * as assert from 'assert'
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vsc from 'vscode'
-import { VarTemplate } from '../src/templates/varTemplates'
+// import { VarTemplate } from '../src/templates/varTemplates'
 import { getCurrentSuggestion } from '../src/postfixCompletionProvider'
 import { getCurrentDelay, delay } from './utils'
 
