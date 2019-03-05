21960bdf9d32be65ab64ee9a5b959d0a2065a6eb
filwaline <skywalker950925@gmail.com>
2019-03-05 15:37:22 +0800

    switch to python and remove some not suitable js/ts postfix templates

--- src/templates/ifTemplates.ts
+++ src/templates/ifTemplates.ts
@@ -8,46 +8,46 @@ export class IfTemplate extends BaseExpressionTemplate {
   buildCompletionItem (code: string, position: vsc.Position) {
     return CompletionItemBuilder
       .create('if', code)
-      .description(`if (expr)`)
-      .replace(`if ({{expr}}) {\n${getIndentCharacters()}\${0}\n}`, position, true)
+      .description(`if expr:`)
+      .replace(`if {{expr}}: \n${getIndentCharacters()}\${0}\n`, position, true)
       .build()
   }
 }
 
-export class ElseTemplate extends BaseExpressionTemplate {
-  buildCompletionItem (code: string, position: vsc.Position, node: ts.Node) {
-    let replacement = '{{expr}}'
-    if (node.parent.kind === ts.SyntaxKind.BinaryExpression) {
-      replacement = `(${replacement})`
-    }
+// export class ElseTemplate extends BaseExpressionTemplate {
+//   buildCompletionItem (code: string, position: vsc.Position, node: ts.Node) {
+//     let replacement = '{{expr}}'
+//     if (node.parent.kind === ts.SyntaxKind.BinaryExpression) {
+//       replacement = `(${replacement})`
+//     }
 
-    return CompletionItemBuilder
-      .create('else', code)
-      .description(`if (!expr)`)
-      .replace(`if (!${replacement}) {\n${getIndentCharacters()}\${0}\n}`, position, true)
-      .build()
-  }
-}
+//     return CompletionItemBuilder
+//       .create('else', code)
+//       .description(`if (!expr)`)
+//       .replace(`if (!${replacement}) {\n${getIndentCharacters()}\${0}\n}`, position, true)
+//       .build()
+//   }
+// }
 
-export class IfEqualityTemplate extends BaseExpressionTemplate {
-  constructor (private keyword: string, private operator: string, private operand: string) {
-    super()
-  }
+// export class IfEqualityTemplate extends BaseExpressionTemplate {
+//   constructor (private keyword: string, private operator: string, private operand: string) {
+//     super()
+//   }
 
-  buildCompletionItem (code: string, position: vsc.Position) {
-    return CompletionItemBuilder
-      .create(this.keyword, code)
-      .description(`if (expr ${this.operator} ${this.operand})`)
-      .replace(`if ({{expr}} ${this.operator} ${this.operand}) {\n${getIndentCharacters()}\${0}\n}`, position, true)
-      .build()
-  }
-}
+//   buildCompletionItem (code: string, position: vsc.Position) {
+//     return CompletionItemBuilder
+//       .create(this.keyword, code)
+//       .description(`if (expr ${this.operator} ${this.operand})`)
+//       .replace(`if ({{expr}} ${this.operator} ${this.operand}) {\n${getIndentCharacters()}\${0}\n}`, position, true)
+//       .build()
+//   }
+// }
 
 export const build = () => [
   new IfTemplate(),
-  new ElseTemplate(),
-  new IfEqualityTemplate('null', '===', null),
-  new IfEqualityTemplate('notnull', '!==', null),
-  new IfEqualityTemplate('undefined', '===', undefined),
-  new IfEqualityTemplate('notundefined', '!==', undefined)
+  // new ElseTemplate(),
+  // new IfEqualityTemplate('null', '===', null),
+  // new IfEqualityTemplate('notnull', '!==', null),
+  // new IfEqualityTemplate('undefined', '===', undefined),
+  // new IfEqualityTemplate('notundefined', '!==', undefined)
 ]
